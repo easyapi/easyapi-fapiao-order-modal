6528f784cf2f3a6345b3afc2c389facaa2f36f7b
leida <zhang20084@126.com>
2020-08-13 16:38:34 +0800

    fix:增加个人抬头修改功能

--- js/order.js
+++ js/order.js
@@ -2,8 +2,7 @@ var vm = new Vue({
   el: '#app',
   data() {
     return {
-      current: 0,
-      willShow: true,
+      invoiceType: '企业',
       dropDownShow: false,
       username: '',
       accessToken: '',
@@ -19,8 +18,7 @@ var vm = new Vue({
       purchaserBankAccount: '',
       companyId: '',
       request: {},
-      searchList: [],
-      sendType: ''
+      companyList: [],
     }
   },
   created() {
@@ -33,7 +31,7 @@ var vm = new Vue({
     //获取税号
     this.taxNumber = this.GetQueryString("taxNumber");
     localStorage.setItem("taxNumber", this.taxNumber);
-    this.searchCompanies();
+    this.getCompanyList();
     this.getUser();
   },
   methods: {
@@ -60,15 +58,17 @@ var vm = new Vue({
       return null;
     },
     // 切换个人与单位
-    tabChange(index) {
-      this.current = index;
-      index == 0 ? this.sendType = "企业" : this.sendType = "个人";
-      if (this.willShow == true && this.sendType == "个人") {
-        this.willShow = false;
-      } else if (this.willShow == false && this.sendType == "企业") {
-        this.willShow = true;
+    tabChange(invoiceType) {
+      this.invoiceType = invoiceType;
+      if (this.invoiceType == '个人') {
+        this.purchaserName = "个人";
       }
-      console.log(this.sendType);
+      else if (this.invoiceType == '企业') {
+        this.getCompanyList();
+        if (this.companyList.length > 0) {
+          this.chooseCompanyTitle(0);
+        }
+      }
     },
     // 企业抬头查询
     searchCompanyTitleList() {
@@ -80,7 +80,7 @@ var vm = new Vue({
             name: this.purchaserName
           }
         }).then(res => {
-          this.searchList = res.data.content;
+          this.companyList = res.data.content;
           this.dropDownShow = true;
         }).catch(error => {
           console.log(error)
@@ -88,12 +88,12 @@ var vm = new Vue({
       }
     },
     chooseCompanyTitle(index) {
-      this.purchaserName = this.searchList[index].name;
-      this.purchaserTaxpayerNumber = this.searchList[index].taxNumber;
-      this.address = this.searchList[index].address;
-      this.phone = this.searchList[index].phone;
-      this.purchaserBank = this.searchList[index].bank;
-      this.purchaserBankAccount = this.searchList[index].bankAccount;
+      this.purchaserName = this.companyList[index].name;
+      this.purchaserTaxpayerNumber = this.companyList[index].taxNumber;
+      this.address = this.companyList[index].address;
+      this.phone = this.companyList[index].phone;
+      this.purchaserBank = this.companyList[index].bank;
+      this.purchaserBankAccount = this.companyList[index].bankAccount;
       this.dropDownShow = false;
     },
     // 获取用户默认的抬头、接受方式、抬头类型 信息
@@ -115,36 +115,20 @@ var vm = new Vue({
         this.phone = res.data.content.company.phone;
         this.purchaserBank = res.data.content.company.bank;
         this.purchaserBankAccount = res.data.content.company.bankAccount;
-        if (this.type == "企业") {
-          this.current = 0;
-          this.willShow = true;
-        } else {
-          this.willShow = false;
-          this.current = 1;
-        }
         this.companyId = res.data.content.company.companyId;
       }).catch(error => {
         console.log(error)
       });
     },
     // 获取发票公司抬头列表
-    searchCompanies() {
+    getCompanyList() {
       axios.get("https://fapiao-api.easyapi.com/companies", {
         params: {
           accessToken: this.accessToken,
           username: this.username
         }
       }).then(res => {
-        let defaultList = res.data.content;
-        for (let k in defaultList) {
-          defaultList[k].name = defaultList[k].name;
-          defaultList[k].taxNumber = defaultList[k].taxNumber;
-          defaultList[k].address = defaultList[k].address;
-          defaultList[k].phone = defaultList[k].phone;
-          defaultList[k].bank = defaultList[k].bank;
-          defaultList[k].bankAccount = defaultList[k].bankAccount;
-        }
-        this.searchList = defaultList;
+        this.companyList = res.data.content;
       }).catch(error => {
         console.log(error)
       });
@@ -173,7 +157,7 @@ var vm = new Vue({
       this.request.username = this.username;
       this.request.accessToken = this.accessToken;
       this.request.taxNumber = this.taxNumber;
-      this.request.type = this.sendType;
+      this.request.type = this.invoiceType;
       this.request.category = "增值税电子普通发票";
       this.request.contentType = "商品明细";
       this.request.property = "电子";
